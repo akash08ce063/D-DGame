ae79e3bb82a57c767a4eda8699eb05c196571d74
HarveyMoMo <mr.jiahanxin@gmail.com>
2014-03-06 16:10:16 -0500

    chang initcharacterandmappanel

--- src/com/game/ui/views/MainPanel.java
+++ src/com/game/ui/views/MainPanel.java
@@ -7,7 +7,9 @@
 package com.game.ui.views;
 
 import com.game.models.GameBean;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -19,6 +21,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 /**
  * This class is used to generate UI for MainPanel and implement the 
@@ -34,38 +37,49 @@ public class MainPanel extends JFrame implements ActionListener {
      * initiate the UI for Main Panel
      */
     public void initUI() {
-        JPanel basicPanel = new JPanel();
+        
         JPanel functionPanel = new JPanel();
         functionPanel.setLayout(new BoxLayout(functionPanel, BoxLayout.Y_AXIS));
+        JPanel buttonPanels[] = new JPanel[4];
         
-        JButton startGame = new JButton("Strat");
+        
+        buttonPanels[0] = new JPanel();
+        JButton startGame = new JButton("Start");
+        startGame.setPreferredSize(new Dimension(120,30));
         startGame.setActionCommand("0");
         startGame.addActionListener(this);
+        buttonPanels[0].add(startGame);
         
-        JButton creatCharater = new JButton("Creat \n" + "Character");
+        buttonPanels[1] = new JPanel();
+        JButton creatCharater = new JButton("Create Player");
         creatCharater.setActionCommand("1");
         creatCharater.addActionListener(this);
+        creatCharater.setPreferredSize(new Dimension(120,30));
+        buttonPanels[1].add(creatCharater);
         
-        JButton mapBuilder = new JButton("Map \n" + "Builder");
+        buttonPanels[2] = new JPanel();
+        JButton mapBuilder = new JButton("Map Builder");
+        mapBuilder.setPreferredSize(new Dimension(120,30));
         mapBuilder.setActionCommand("2");
         mapBuilder.addActionListener(this);
+        buttonPanels[2].add(mapBuilder);
         
+        buttonPanels[3] = new JPanel();
         JButton exitGame = new JButton("exit");
         exitGame.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent event) {
              System.exit(0);
             }
         });
+        exitGame.setPreferredSize(new Dimension(120,30));
+        buttonPanels[3].add(exitGame);
         
-        functionPanel.add(startGame);
-        functionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
-        functionPanel.add(creatCharater);
-        functionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
-        functionPanel.add(mapBuilder);
-        functionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
-        functionPanel.add(exitGame);
-        basicPanel.add(functionPanel);
-        add(basicPanel);
+        functionPanel.add(buttonPanels[0]);
+        functionPanel.add(buttonPanels[1]);
+        functionPanel.add(buttonPanels[2]);
+        functionPanel.add(buttonPanels[3]);
+        functionPanel.setBorder(new EmptyBorder(new Insets(50, 30, 10, 10)));
+        add(functionPanel);
         
         setTitle("SelectCharatersAndMap");
         setSize(500, 450);
